be351712a226419f27e9b512346ada27027d541d
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2017-04-27 13:58:32 -0700

    got legend to work

--- PlotDynamicsErrors.py
+++ PlotDynamicsErrors.py
@@ -6,19 +6,21 @@ import numpy as np
 
 H_depths = [1, 5, 10];
 colors = ["r", "g", "b"];
-axs = []
-patches = []
+datasets = [];
+ax = None;
 labels = []
 
 for H_depth, color in zip(H_depths, colors):
     data = pickle.load(open("norm_errs_H" + str(H_depth) + ".pkl", "rb"));
+    datasets.append(data);
     labels.append("horizon = " + str(H_depth));
-    patches.append(mpatches.Patch(color=color, label=labels[-1]));
-    axs.append(sns.tsplot(data, color=color));
-    axs[-1].legend([patches[-1]]);
 
-#plt.legend(handles=patches);
-plt.legend();
+bigdata = np.zeros((datasets[0].shape[0], datasets[0].shape[1], len(datasets)));
+for ii, data in enumerate(datasets):
+    bigdata[:, :, ii] = data;
+
+sns.set(font_scale=1.5);
+sns.tsplot(data=bigdata, condition=labels, legend=True);
 plt.title("L2 norm of errors as a function of timestep");
 plt.xlabel("Timestep");
 plt.ylabel("L2 difference between true and estimated state");

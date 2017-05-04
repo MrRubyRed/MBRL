9634eb214fcabcd1e57f9db7efaca25b8e8a0bee
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2017-05-04 09:31:49 -0700

    found the bug. was not indexing targets correctly

--- mbrl/dynamics/dynamics.py
+++ mbrl/dynamics/dynamics.py
@@ -11,7 +11,8 @@ class Dynamics:
 
     # Constructor.
     def __init__(self, state_dim = None, action_dim = None, layer_sizes = None):
-        self.sa_ = tf.placeholder(tf.float32, shape=(None, state_dim + action_dim), name="sa")
+        self.state_ = tf.placeholder(tf.float32, shape=(None, state_dim), name="state")
+        self.action_ = tf.placeholder(tf.float32, shape=(None, action_dim), name="action")
         self.next_state_ = tf.placeholder(tf.float32, shape=(None, state_dim), name="next_state")
 
         # Lists to store weights, biases, and layers. Also keep track of an L1 regularization term.
@@ -23,20 +24,25 @@ class Dynamics:
         # Build up weights, biases, and regularization.
         for ii in range(len(layer_sizes) - 1):
             self.weights_.append(
-                0.1 * tf.Variable(tf.random_uniform([layer_sizes[ii], layer_sizes[ii + 1]],
-                                                    -1.0, 1.0, dtype=tf.float32),
-                                  name="H" + str(ii)))
+                tf.Variable(tf.random_uniform([layer_sizes[ii], layer_sizes[ii + 1]],
+                                              -0.1, 0.1, dtype=tf.float32),
+                            name="H" + str(ii)))
+
             self.biases_.append(
-                0.1 * tf.Variable(tf.random_uniform([1, layer_sizes[ii + 1]],
-                                                    -1.0, 1.0, dtype=tf.float32),
-                                  name="B" + str(ii)))
+                tf.Variable(tf.random_uniform([1, layer_sizes[ii + 1]],
+                                               -0.1, 0.1, dtype=tf.float32),
+                            name="B" + str(ii)))
+
             self.reg_ += (tf.reduce_sum(tf.abs(self.weights_[-1])) +
                           tf.reduce_sum(tf.abs(self.biases_[-1])))
 
         # Generate layers.
         self.layers_.append(
-            tf.nn.sigmoid(tf.add(tf.matmul(self.sa_, self.weights_[0]), self.biases_[0]),
+            tf.nn.sigmoid(tf.add(tf.add(tf.matmul(self.state_, self.weights_[0][:state_dim, :]),
+                                        tf.matmul(self.action_, self.weights_[0][state_dim:, :])),
+                                 self.biases_[0]),
                           name="A" + str(0)))
+
         for ii in range(len(self.weights_) - 2):
             self.layers_.append(
                 tf.nn.sigmoid(tf.add(tf.matmul(self.layers_[-1], self.weights_[ii + 1]),
@@ -45,7 +51,7 @@ class Dynamics:
 
         self.layers_.append(
             tf.add(tf.matmul(self.layers_[-1], self.weights_[-1]), self.biases_[-1],
-                   name="A_end"))
+            name="A_end"))
 
         # Set output layer.
         self.output_ = self.layers_[-1]
--- sandbox/System_ID.py
+++ sandbox/System_ID.py
@@ -20,12 +20,10 @@ ACTION_DIM = 1
 dyn = Dynamics(STATE_DIM, ACTION_DIM, [STATE_DIM + ACTION_DIM, 20, 10, STATE_DIM])
 
 # Create loss functor (RMS error) and set up optimizer.
-rms = tf.sqrt(tf.reduce_mean(tf.reduce_sum(tf.square(
-    tf.subtract(dyn.next_state_, dyn.output_)), 1, keep_dims=True)))
+rms = tf.sqrt(tf.reduce_mean(tf.reduce_sum(tf.square(tf.subtract(dyn.next_state_, dyn.output_)), 1, keep_dims=True)))
 learning_rate = 0.01
 momentum = 0.995
-train_step = tf.train.MomentumOptimizer(
-    learning_rate=learning_rate, momentum=momentum).minimize(rms)
+train_step = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=momentum).minimize(rms)
 
 # Set up tf session.
 sess = tf.Session()
@@ -36,30 +34,33 @@ sess.run(init)
 NUM_ROLLOUTS = 300
 TIME_HORIZON = 20
 
-sa = np.zeros((NUM_ROLLOUTS * TIME_HORIZON, STATE_DIM + ACTION_DIM))
+states = np.zeros((NUM_ROLLOUTS * TIME_HORIZON, STATE_DIM))
+actions = np.zeros((NUM_ROLLOUTS * TIME_HORIZON, ACTION_DIM))
 targets = np.zeros((NUM_ROLLOUTS * TIME_HORIZON, STATE_DIM))
 for ii in range(NUM_ROLLOUTS):
     s = env.reset()
 
     for jj in range(TIME_HORIZON):
         a = np.random.uniform(-2.0, 2.0, (1,))
-        sa[ii*TIME_HORIZON + jj, :STATE_DIM] = s
-        sa[ii*TIME_HORIZON + jj, STATE_DIM:] = a
+        states[ii*TIME_HORIZON + jj, :] = s
+        actions[ii*TIME_HORIZON + jj, :] = a
 
         step = env.step(a)
-        targets[ii * TIME_HORIZON, :] = step[0]
+        targets[ii * TIME_HORIZON + jj, :] = step[0]
         s = step[0]
 
 # Training.
 BATCH_SIZE = 20
-NUM_TRAIN_STEPS = 1000
+NUM_TRAIN_STEPS = 10000
 for ii in range(NUM_TRAIN_STEPS):
     indices = np.random.randint(NUM_ROLLOUTS * TIME_HORIZON, size=BATCH_SIZE)
 
-    sess.run(train_step, feed_dict={dyn.sa_ : sa[indices],
+    sess.run(train_step, feed_dict={dyn.state_ : states[indices],
+                                    dyn.action_ : actions[indices],
                                     dyn.next_state_ : targets[indices]})
 
     if np.mod(ii, 50) == 0:
         print("RMS error at iteration %d is %f" %
-                  (ii, sess.run(rms, feed_dict={dyn.sa_ : sa,
+                  (ii, sess.run(rms, feed_dict={dyn.state_ : states,
+                                                dyn.action_ : actions,
                                                 dyn.next_state_ : targets})))
